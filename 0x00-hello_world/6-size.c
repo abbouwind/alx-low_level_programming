80d80f7432961b4cdbed129045286b77e4e5853f
abbouwind <mabbaoui22@gmail.com>
2025-05-14 08:05:38 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,15 @@
  */
 int main(void)
 {
-printf("Size of an int: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of an int: %lu byte(s)\n", sizeof(long int));
-printf("Size of an int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of an int: %lu byte(s)\n", sizeof(float));
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+printf("Size of an int: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of an int: %lu byte(s)\n", sizeof(li));
+printf("Size of an int: %lu byte(s)\n", sizeof(lli));
+printf("Size of an int: %lu byte(s)\n", sizeof(f));
 return (0);
 }
