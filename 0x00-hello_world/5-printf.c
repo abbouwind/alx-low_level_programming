6fe4162d06d11fa6d71d23b25352d61d5112f92d
abbouwind <mabbaoui22@gmail.com>
2025-05-14 07:16:33 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+/**
+*main is a function that prints a line followed by a new line  using the printf fuction
+*
+*return (0) = success
+*
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
