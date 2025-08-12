425d4897ec30d8fc2e7ac1f8889ab094de3f9678
abbouwind <mabbaoui22@gmail.com>
2025-08-12 06:13:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Prints a sentence about programming
+ *
+ * Description: This program uses puts to print the message
+ * "Programming is like building a multilingual puzzle"
+ * starting with a double quote and followed by a new line.
+ * Return: Always 0 (Success)
+ */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
 
