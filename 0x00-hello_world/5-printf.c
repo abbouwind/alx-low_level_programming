0820283706dc742937d96e18d91d1ac0cd2b9f9d
abbouwind <mabbaoui22@gmail.com>
2025-08-12 06:20:37 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * funtion main prints the following string "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line using \n escape character as printf function requires it to add a new line 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
