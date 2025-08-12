144feb2057aba3ee7dd42707e1bd43084b502e73
abbouwind <mabbaoui22@gmail.com>
2025-08-12 06:39:43 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-	printf("size of a char: ", sizeof(char);
-	printf("Size of an int: ", sizeof(int);
-	printf("Size of a long int: ", sizeof(long);
-	printf("Size of a long long int: ", sizeof(long long);
-	printf("Size of float: ", sizeof(float);
+	printf("size of a char: ", sizeof(char));
+	printf("Size of an int: ", sizeof(int));
+	printf("Size of a long int: ", sizeof(long));
+	printf("Size of a long long int: ", sizeof(long long));
+	printf("Size of float: ", sizeof(float));
 	return (0);
 }
