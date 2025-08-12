fb392fbfa17829ad01397fd4c31baf87814cc258
abbouwind <mabbaoui22@gmail.com>
2025-08-12 06:22:34 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /*
- * funtion main prints the following string "with proper grammar, but the outcome is a piece of art,"
- * followed by a new line using \n escape character as printf function requires it to add a new line 
+ * funtion main prints the following string
+ *  "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line using \n escape character
+ * as printf function requires it to add a new line
  */
 int main(void)
 {
