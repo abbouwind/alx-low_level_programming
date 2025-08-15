df676801649d68fde4ffa2e207945760ef0abd9b
abbouwind <mabbaoui22@gmail.com>
2025-08-15 04:05:08 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+      int up = 65;
+      int low = 97;
+      while (low <= 122 )
+      {
+            putchar(low);
+            low += 1;
+      }
+      while (up <= 90)
+      {
+            putchar(up);
+            up += 1;
+      }
+      return (0);
+}
