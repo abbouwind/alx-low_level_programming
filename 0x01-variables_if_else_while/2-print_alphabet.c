d93dd2aaab4a3575125b97ce2f756a65d4ba21f2
abbouwind <mabbaoui22@gmail.com>
2025-08-16 05:52:07 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int low = 97;
+	while ( low <= 122 )
+	{
+		putchar( low );
+		low += 1;
+	}
+      
+	putchar('\n');
+      
+	return (0);
+}
