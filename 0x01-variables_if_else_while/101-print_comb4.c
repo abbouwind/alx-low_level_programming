21a568bcd7d4c863bfc751b0fa2b1ff8060f9555
abbouwind <mabbaoui22@gmail.com>
2025-08-21 05:50:21 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a, b, c;
+	for( a = 48; a <= 55; a++)
+	{
+		for( b = a + 1; b <= 56; b++)
+		{
+			for ( c = b + 1; c <= 57; c++)
+			{
+				if (c > 50)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				putchar(a);
+				putchar(b);
+				putchar(c);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
