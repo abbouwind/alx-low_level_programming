b8a5425ace091af938d773231b35ae91021572d5
abbouwind <mabbaoui22@gmail.com>
2025-08-21 04:42:40 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		if (i > 48)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
