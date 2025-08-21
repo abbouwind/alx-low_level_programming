9ab5671aba7bd16950e7968e9101f6d8d5328bb2
abbouwind <mabbaoui22@gmail.com>
2025-08-21 04:53:55 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-	for (int a = 48; a <= 56; a++)
+	for(int a = 48; a <= 56; a++)
 	{
-		for (int b = a+1; b <= 57; b++)
+		for(int b = a+1; b <= 57; b++)
 		{
 			if (b>'1')
 			{
