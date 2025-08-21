cfab4ad56d40f5b97336ba6463de13da1b720bda
abbouwind <mabbaoui22@gmail.com>
2025-08-21 04:36:43 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-	char i = '1';
+	char i = '0';
 	char j = 'a';
 
-	while(i <= '9')
+	while (i <= '9')
 	{
 		putchar(i);
 		i++;
 	}
-	while(j <= 'f')
+	while (j <= 'f')
 	{
 		putchar(j);
 		j++;
