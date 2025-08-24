55d1cfb61e1efc020b103491963fa12eae256c45
abbouwind <mabbaoui22@gmail.com>
2025-08-24 09:02:09 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a number
+ * @i: the number to check
+ * Return: absolute value of the number is
+ * */
+
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else 
+	{
+		return (-a);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,6 @@ void print_alphabet_x10(void);
 int _islower(int f);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 
 #endif
