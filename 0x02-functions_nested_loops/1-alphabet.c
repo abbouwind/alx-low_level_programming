84115708525754108f9f9e07a70a862ac2d63c5c
abbouwind <mabbaoui22@gmail.com>
2025-08-24 04:36:00 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
-*main - Entry point
-*Return: Always 0 (Success)
+*print_alphabet - a function that prints the alphabet
+*using _putchar function
 */
 
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
