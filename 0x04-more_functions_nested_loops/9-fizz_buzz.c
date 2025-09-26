b6dd3cfec400a8eb1aa0bd0ecb99c257cdb7932d
abbouwind <mabbaoui22@gmail.com>
2025-09-26 12:30:30 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *print numbers from 1 to 100
+ *if number is multiple of 3 replace wih Fizz
+ *if number is multiple of 5 replace wih Buzz
+ *if number is multiple of 3 and 5
+ *replace wih FizzBuzz
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return (0);
+}
