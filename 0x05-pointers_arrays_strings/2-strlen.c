baf1eaffb993b89e57561817676f9437f651c955
abbouwind <mabbaoui22@gmail.com>
2025-10-01 10:02:31 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,18 +3,19 @@
 /**
  * _strlen - a function that
  * returns the lenght of a string
+ * @s: the string
  * Return: the lenght of string
  */
 
 int _strlen(char *s)
 {
 	int i, lenght;
+
 	i = lenght = 0;
-	
 	while (s[i] != '\0')
 	{
-		lenght+=1;
-		i+=1;
+		lenght += 1;
+		i += 1;
 	}
 	return (lenght);
 }
